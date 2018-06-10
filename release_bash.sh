b44b5bcd09f73ece3d69880ca61eeb6ae8f6c4df
Alexander Martinez <alexmtnezf@gmail.com>
2018-06-10 13:06:37 -0500

    Enabling migrations creations in release_bash.sh script, used in Procfile release stage of Heroku deployment process.

--- release_bash.sh
+++ release_bash.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd src/
 python manage.py collectstatic --noinput
+python manage.py makemigrations
 python manage.py migrate --noinput
 # Run this only in your first deployment
 #python manage.py createsuperuser --noinput --name admin --email admin@example.com
