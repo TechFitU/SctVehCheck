f0bef9741b45077db3113c4c7ea0651b329ddd5f
Alexander Martinez <alexmtnezf@gmail.com>
2018-06-11 15:32:35 -0500

    Updated production.py

--- src/sctvehcheck/settings/production.py
+++ src/sctvehcheck/settings/production.py
@@ -90,8 +90,6 @@ LOGGING = {
 logging.config.dictConfig(LOGGING)
 import os
 import django_heroku
-import dj_database_url
-
 
 static_files = os.environ['ENV'] != 'development'
 # override DATABASE_URL set by django_heroku because it forces SSL mode locally
@@ -100,9 +98,9 @@ ssl_require = os.environ['ENV'] != 'development'
 # Activate Django-Heroku configuration system.
 # Docs: https://devcenter.heroku.com/articles/django-app-configuration
 django_heroku.settings(locals(), staticfiles=static_files)
-
-
-locals()['DATABASES']['default'] = dj_database_url.config(
-    conn_max_age=django_heroku.MAX_CONN_AGE, ssl_require=ssl_require)
-
-print(locals()['DATABASES']['default'])
+#
+#
+# locals()['DATABASES']['default'] = dj_database_url.config(
+#     conn_max_age=django_heroku.MAX_CONN_AGE, ssl_require=ssl_require)
+#
+# print(locals()['DATABASES']['default'])
